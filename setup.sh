0dd18e7df7f2fee24e35c76e75f347bef7b810ce
Samuel Showalter <samuelrshowalter@gmail.com>
2022-10-02 13:35:25 -0700

    getting rid of a final file

--- setup.sh
+++
@@ -1,4 +0,0 @@
-
-# Read in files
-wget http://cs.stanford.edu/people/karpathy/char-rnn/shakespeare_input.txt -P ./data
-wget https://raw.githubusercontent.com/stacks/stacks-project/master/stacks.tex -P ./data
